87cdc6cbdc62ae9748e84783b8ea5952cdf8aa1d
alvinhu182 <alvinhu@hotmail.com>
2022-11-03 21:21:25 -0300

    projeto iniciado

---
+++ app.ts
@@ -0,0 +1,31 @@
+import AdminJS from "adminjs";
+import AdminJSExpress from '@adminjs/express';
+import express from 'express';
+require('dotenv').config()
+
+const PORT =  process.env.PORT_HOST;
+
+const start = async () => {
+    const adminOptions = {
+        rootPath: '/admin',
+        dashboard: {
+
+        },
+        branding: {
+            logo: 'https://artpoin.com/wp-content/uploads/2020/02/Sem-t%C3%ADtulo-2.png' ,
+            companyName: 'CandyShop',
+            favicon:'https://artpoin.com/wp-content/uploads/2020/02/Sem-t%C3%ADtulo-2.png'
+        }
+    }
+    const app = express();
+    const admin = new AdminJS(adminOptions);
+
+    const adminRouter = AdminJSExpress.buildRouter(admin);
+    app.use(admin.options.rootPath, adminRouter)
+
+    app.listen(PORT, () => {
+        console.log("projeto topzera")
+    })
+}
+
+start();
